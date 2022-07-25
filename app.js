06e839716b31b0738192930626a3292dad2f35da
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-07-25 21:20:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Here we learn about Let and Const 
+
+   // Let :- Ek baar value define kar diye to vlue change
+      // Kar sakte hai
+
+  // But in Const :- Ek baar value define kar diye to value change 
+    // hahi kar sake hai..
+
+
+
+     // *** Arrow function :- 
+
+      function sayhello(){
+          console.log("Samrat");
+
+      }
+      sayname();
